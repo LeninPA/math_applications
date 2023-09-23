25380b3a9d9ca7ab77873e3415822c583f0f6234
LeninPA <lenin.pavon@comunidad.unam.mx>
2023-09-22 19:55:41 -0600

    FIX: Corrección de parámetros en covid_mex.jl

--- complex_systems/covid_mex.jl
+++ complex_systems/covid_mex.jl
@@ -8,23 +8,10 @@ using Distributions
 using Random
 Random.seed!(12459641728)
 # Importación de los datos
+# TODO: Que reconozca los datos en Windows
 df = DataFrame(CSV.File("./casos_diarios_2.dat", delim="\t\t", header = false,select=[2]))
 rename!(df,:Column2 => :Casos)
-# Obtención de casos totales
-df[!,"Totales"] .= df[!,"Casos"]
-for (i, row) in enumerate(eachrow(df[2:end,:]))
-    df[i+1,"Totales"] += df[i,"Totales"]
-end
-# Graficación
-df[!,"Normal"] .= 3*df.Casos ./ maximum(df.Casos)
-scatter(
-    1:195,
-    df.Normal,
-    label = "Casos septiempre",
-    markercolor=:pink,
-    xlabel = "Días",
-    ylabel = "Casos registrados"
-)
+
 scatter(
     1:195,
     df.Casos,
@@ -33,38 +20,6 @@ scatter(
     xlabel="Días",
     ylabel="Casos registrados"
 )
-# Log-normal = (A/x)exp(-(log(x)-mu)/(sigma^2))
-# Primera aproximación
-# average   = mean(df.Casos)
-# deviation = std(df.Casos)
-# function log_normal(x, average = 0.0, deviation = 1.0, normalization = 1.0)
-#    return (normalization*exp( -(log(x)-average)^2 /deviation^2 ))/x
-# end
-# function ayuda(x::Vector, average=0.0, deviation=1.0, normalization=1.0)
-#     return (normalization .* exp.(.-(log.(x) .- average).^2 ./ deviation.^2)) ./ x
-# end
-# # function log_normal(x::Array, average::float=0.0, deviation::float=1.0, normalization::float=1.0)
-# #     x = (normalization ./ x).*exp.(.-(log.(x) .- average) ./ (deviation.^2))
-# # end
-# for elem in [1,50,100,150,200]
-#     println("$elem ", log_normal(elem, 0, 0.5, 10000000))
-# end
-# plot!(
-#     0:195,
-#     ayuda([0:195...],150.0, 150.0,8000.0),
-#     label="Casos septiempre"
-# )
-
-# function log_normal(x, average = 0.0, deviation = 1.0, normalization = 1.0)
-#    return normalization*(1/x)*(exp(-(log(x)-average)^2 / (deviation^2)))
-# end
-# #println(map(log_normal(average = 0, deviation = 0.6, normalization = 100), [1,2,3]))
-# # for elem in [1:50]:
-
-# function log_normal(x, average=0.0, deviation=1.0, normalization=1.0)
-#     a = Distributions.LogNormal(average, deviation)
-#     return normalization * Distributions.pdf(a,x)
-# end
 
 function log_normal(x, average=0.0, deviation=1.0, normalization=1.0)
     return (normalization/x)*exp(-((log(x)-average)/deviation)^2) 
@@ -72,9 +27,9 @@ end
 
 #a = Distributions.LogNormal(1000,0.5)
 r = 1:195
-n_ini = 6000
-a_ini = 1000
-s_ini = 200
+n_ini = 999999
+a_ini = 5.2
+s_ini = 0.7
 plot!(
     r,
     [log_normal(x,a_ini,s_ini,n_ini) for x in r],  
@@ -85,7 +40,7 @@ function distancia(x_1,x_2)
     return (x_1-x_2)^2
 end
 
-function alg_gen(epsilon, datos, func, avg, dev, r, norm, k=50000)
+function alg_gen(epsilon, datos, func, avg, dev, r, norm, k=1000000)
     n = length(datos)
     for i in 1:k
         prediccion = [func(x,avg,dev,norm) for x in r]
@@ -109,25 +64,11 @@ function alg_gen(epsilon, datos, func, avg, dev, r, norm, k=50000)
     return (norm,avg, dev)
 end
 
-(n,a,s) = alg_gen(0.0001,df.Casos,log_normal,150, 1.6664133995196326,r,6000)
+(n, a, s) = alg_gen(0.0001, df.Casos, log_normal, a_ini, s_ini, r, n_ini)
 print("$n $a $s")
-# plot!(
-#     r,
-#     [n*Distributions.pdf(LogNormal(a, s), x) for x in r],  
-#     label = "Final"
-# )
-# plot!(
-#     r,
-#     [3185 * n * Distributions.pdf(LogNormal(a, s), x) for x in r],
-#     label="Final 2"
-# )
-# plot!(
-#     r,
-#     [n * Distributions.pdf(LogNormal(a, s), x) for x in r],
-#     label="Final"
-# )
+
 plot!(
     r,
     [log_normal(x,a,s,n) for x in r],
-    label="Final 2"
+    label="Ajuste"
 )
