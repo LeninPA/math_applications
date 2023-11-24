28881bad622e66d1d4072ff3671177a24b5254ac
LeninPA <lenin.pavon@comunidad.unam.mx>
2023-11-24 00:13:13 -0600

    FEAT: TSNE para visualización básica

--- complex_systems/redes/redes.jl
+++ complex_systems/redes/redes.jl
@@ -1,17 +1,31 @@
+# using PlotlyJS, LightGraphs
+# import GraphPlot  # for spring_layout
+using SGtSNEpi, Random
+using Graphs
+using GLMakie
+GLMakie.activate!()
+
 v = [] # vértices
 s = 0 # susceptibilidad
 f = 0 # fortaleza
 
+"""
+	Red
+
+	Crea una red de nodos de una gráfica, por defecto dirigida
+"""
 mutable struct Red
-	nodos::list
+	nodos :: Vector{Int}
 end
 
 mutable struct Nodo
-	v :: list
-	s :: double
-	f :: double
+	# neighboring vertices
+	v :: Vector{Int}
+	# state
+	s :: Int
+	# susceptibility
+	f :: Float64
 end
-
 function Contains(i::Nodo)
 	return true
 end
@@ -22,13 +36,14 @@ function AddVecino(i::Nodo, j::Nodo)
 	else
 		append!(v,[i])
 		return true
+	end
 end
 
 function getNumVecinos(i::Nodo)
 	return length(i.v)
 end
 
-function getVecino(n::Nodo, i::int)
+function getVecino(n::Nodo, i::Int)
 	return n.v[i]
 end
 
@@ -36,7 +51,7 @@ function setEstado!(n::Nodo, estado)
 	n.s = estado
 end
 
-function crearRed(k::int)
+function crearRed(k::Int)
 	r # número aleatorio
 	for l in 1:k
 		# crear nodo con listas vacías
@@ -59,8 +74,57 @@ function verRed(R::Red)
 	# TODO: Aprender a usar Networks en julia
 end
 
-function guardarRed(R::Red, formato::str)
+function guardarRed(R::Red, formato::String)
 	# TODO: Aprender a usar archivos en julia
 end
 
+# println("uwu")
 
+# """
+# No olvidar citar 
+# @inproceedings{pitsianis2019sgtsnepi,
+#     author = {Pitsianis, Nikos and Iliopoulos, Alexandros-Stavros and Floros, Dimitris and Sun, Xiaobai},
+#     doi = {10.1109/HPEC.2019.8916505},
+#     booktitle = {IEEE High Performance Extreme Computing Conference},
+#     month = {11},
+#     title = {{Spaceland Embedding of Sparse Stochastic Graphs}},
+#     year = {2019}
+# }
+# y 
+# @article{bezanson2017julia,
+#   title={Julia: A fresh approach to numerical computing},
+#   author={Bezanson, Jeff and Edelman, Alan and Karpinski, Stefan and Shah, Viral B},
+#   journal={SIAM review},
+#   volume={59},
+#   number={1},
+#   pages={65--98},
+#   year={2017},
+#   publisher={SIAM},
+#   url={https://doi.org/10.1137/141000671}
+# }
+# """
+println("uwu")
+# Generate a random layout
+
+n=1000
+# G = circular_ladder_graph(500)
+G=Graph()
+add_vertices!(G,n)
+m=n-1
+for i in 1:m
+	add_edge!(G,i,i+1)
+end
+is_directed(G)
+for v in vertices(G)
+	println(v)
+end
+for v in edges(G)
+	println(v)
+end
+Y = sgtsnepi(G)
+show_embedding(Y; 
+	A = adjacency_matrix(G),        # show edges on embedding
+    # mrk_size=1,                   # control node sizes
+    # lwd_in=0.01, lwd_out=0.001, # control edge widths
+    # edge_alpha=0.03             # control edge transparency
+)
