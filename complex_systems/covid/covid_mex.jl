7557de054adac17d8578aff64dd2a888cd59cc67
LeninPA <lenin.pavon@comunidad.unam.mx>
2024-01-08 21:51:35 -0600

    FIX: Ruta desde complex_systems

--- complex_systems/covid/covid_mex.jl
+++ complex_systems/covid/covid_mex.jl
@@ -9,8 +9,8 @@ using Random
 Random.seed!(12459641728)
 # Importación de los datos
 # TODO: Que reconozca los datos en Windows
-df = DataFrame(CSV.File("./casos_diarios_2.dat", delim="\t\t", header = false,select=[2]))
-rename!(df,:Column2 => :Casos)
+df = DataFrame(CSV.File("./covid/casos_diarios_2.dat", delim="\t\t", header=false, select=[2]))
+rename!(df, :Column2 => :Casos)
 
 scatter(
     1:195,
@@ -22,7 +22,7 @@ scatter(
 )
 
 function log_normal(x, average=0.0, deviation=1.0, normalization=1.0)
-    return (normalization/x)*exp(-((log(x)-average)/deviation)^2) 
+    return (normalization / x) * exp(-((log(x) - average) / deviation)^2)
 end
 
 #a = Distributions.LogNormal(1000,0.5)
@@ -32,26 +32,26 @@ a_ini = 5.2
 s_ini = 0.7
 plot!(
     r,
-    [log_normal(x,a_ini,s_ini,n_ini) for x in r],  
-    label = "Inicial"
+    [log_normal(x, a_ini, s_ini, n_ini) for x in r],
+    label="Inicial"
 )
-    
-function distancia(x_1,x_2)
-    return (x_1-x_2)^2
+
+function distancia(x_1, x_2)
+    return (x_1 - x_2)^2
 end
 
 function alg_gen(epsilon, datos, func, avg, dev, r, norm, k=1000000)
     n = length(datos)
     for i in 1:k
-        prediccion = [func(x,avg,dev,norm) for x in r]
+        prediccion = [func(x, avg, dev, norm) for x in r]
         delta_2 = randn()
         avg_2 = avg + randn()
-        dev_2 = dev + delta_2 > 0 ? dev+delta_2 : dev
+        dev_2 = dev + delta_2 > 0 ? dev + delta_2 : dev
         norm_2 = norm + randn()
         prediccion_nueva = [func(x, avg_2, dev_2, norm_2) for x in r]
-        d1 = sum(distancia.(prediccion, datos))/n
+        d1 = sum(distancia.(prediccion, datos)) / n
         # println(d1)
-        d2 = sum(distancia.(prediccion_nueva, datos))/n
+        d2 = sum(distancia.(prediccion_nueva, datos)) / n
         # println(d2)
         # println(d2)
         # println(d1)
@@ -61,7 +61,7 @@ function alg_gen(epsilon, datos, func, avg, dev, r, norm, k=1000000)
         dev = d2 < d1 ? dev_2 : dev
         norm = d2 < d1 ? norm_2 : norm
     end
-    return (norm,avg, dev)
+    return (norm, avg, dev)
 end
 
 (n, a, s) = alg_gen(0.0001, df.Casos, log_normal, a_ini, s_ini, r, n_ini)
@@ -69,6 +69,6 @@ print("$n $a $s")
 
 plot!(
     r,
-    [log_normal(x,a,s,n) for x in r],
+    [log_normal(x, a, s, n) for x in r],
     label="Ajuste"
 )
